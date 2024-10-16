4ab0ea57d0ccb40587ba864e09452dee759bfecb
dajonss <davidjonsson061@gmail.com>
2024-10-16 11:57:06 +0200

    Update CreateReleaseBranch.py

--- CreateReleaseBranch.py
+++ CreateReleaseBranch.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 
 
 def kör_git_kommando(kommando):
@@ -12,19 +11,21 @@ def kör_git_kommando(kommando):
 
 def öppna_versions_fil():
     with open("release.txt", "r") as fil:
-        version = fil.read()
-        return version
+        currentVersion = fil.read()
+        currentVersion = currentVersion.split(".")  # ["1", "0", "0"]
+        newVersion = int(currentVersion[0]) + 1
+        return f"{newVersion}.0.0"
 
 
-def spara_versions_fil(currentVersion):
-    currentVersion = currentVersion.split(".")  # ["1", "0", "0"]
-    newVersion = int(currentVersion[0]) + 1
+def spara_versions_fil(version):
     with open("release.txt", "w") as fil:
-        fil.write(newVersion)
+        fil.write(version)
 
 
 version = öppna_versions_fil()
 print(f"Nuvarande version: {version}")
+
+spara_versions_fil(version)
 
 print(f"Kör 'branch release-{version}':")
 output = kör_git_kommando(f"git branch release-{version}")
@@ -32,5 +33,3 @@ print(output)
 
 output = kör_git_kommando(f"git push origin release-{version}")
 print(output)
-
-spara_versions_fil()
