93d371ecbc223230710f33f252a5c57aeebc97b4
dajonss <davidjonsson061@gmail.com>
2024-10-16 10:01:09 +0200

    Update [trigger] create rc branch.py

--- [trigger] create rc branch.py
+++ [trigger] create rc branch.py
@@ -1,11 +1,27 @@
-import subprocess, os
-subprocess.run(["ls", "-l"])
-if not (os.path.exists("Diverse")):
-    subprocess.run(["git", "clone", "https://github.com/init-iator/Diverse.git"])
-# os.chdir("C:\chasAcademy\Systemutveckling-Python\pythonGit\Diverse")
-newFolder = os.path.join(os.getcwd(), "Diverse")
-# print(newFolder)
-# subprocess.call("ls", shell=True, cwd=newFolder)
-# subprocess.run(["git", "checkout", "-b", "testbranch"], cwd=newFolder)
-# subprocess.run(["git", "checkout", "-b", "testbranch"])
-subprocess.run(["git", "push", "-u", "origin", "testbranch"], cwd=newFolder,)
+import subprocess
+import os
+# subprocess.run(["ls", "-l"])
+# if not (os.path.exists("Diverse")):
+#     subprocess.run(
+#         ["git", "clone", "https://github.com/init-iator/Diverse.git"])
+# # os.chdir("C:\chasAcademy\Systemutveckling-Python\pythonGit\Diverse")
+# newFolder = os.path.join(os.getcwd(), "Diverse")
+# # print(newFolder)
+# # subprocess.call("ls", shell=True, cwd=newFolder)
+# # subprocess.run(["git", "checkout", "-b", "testbranch"], cwd=newFolder)
+# # subprocess.run(["git", "checkout", "-b", "testbranch"])
+# subprocess.run(["git", "push", "-u", "origin", "testbranch"], cwd=newFolder,)
+
+
+def kör_git_kommando(kommando):
+    try:
+        # Kör git-kommandot med subprocess
+        resultat = subprocess.run(kommando)
+        return resultat.stdout
+    except Exception as error:
+        return f"Ett fel uppstod: {error}"
+
+
+print("Kör 'git status':")
+output = kör_git_kommando("git status")
+print(output)
